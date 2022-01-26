9de47bc63f649a46c3aee60193ac594f1f319955
Jamey Boyett <jamey.boyett@pwrlab.com>
2022-01-26 14:47:54 -0800

    Testing fix

--- src/firefly/infrastructure/repository/abstract_storage_interface.py
+++ src/firefly/infrastructure/repository/abstract_storage_interface.py
@@ -164,29 +164,24 @@ class AbstractStorageInterface(ffd.LoggerAware, ABC):
         print('relationships', relationships)
         if len(relationships.keys()) > 0:
             obj = entity.to_dict(force_all=True, skip=list(relationships.keys()))
-            print('we got obj', obj)
+            print('we got obj',)
             for k, v in relationships.items():
-                print('kv pair', k, v)
                 if v['this_side'] == 'one':
                     try:
-                        print('Hi', getattr(entity, k))
                         obj[k] = getattr(entity, k).id_value()
-                        print('we good')
                     except AttributeError:
-                        print('WE GOT NONE')
                         obj[k] = None
                 elif v['this_side'] == 'many':
                     obj[k] = []
                     for f in getattr(entity, k):
                         try:
-                            print('testing', f)
-                            obj[k].append(f.id_value())
-                            print('testing', f.id_value())
+                            value = f
+                            if not isinstance(f, str):
+                                value = f.id_value()
+                            obj[k].append(value)
                         except AttributeError:
-                            print('Did we lose?')
                             obj[k].append(None)
         else:
-            print('tttttt')
             obj = entity.to_dict(force_all=True)
-        print('obj isssssss', obj)
+
         return self._serializer.serialize(obj)
